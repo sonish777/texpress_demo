90a4d4440a834d781007f18db96ff09356e214b6
Sonish <sonishmaharjan1@gmail.com>
2023-04-20 00:57:02 +0545

    fix: fix redis url

--- packages/shared/src/connections/redis.ts
+++ packages/shared/src/connections/redis.ts
@@ -10,10 +10,9 @@ class RedisConnection {
     private readonly logger: ConsoleLogger = Container.get(ConsoleLogger);
 
     private constructor(public readonly ttl: number) {
+        console.log("(process.env.REDIS_URL):", String(process.env.REDIS_URL))
         this.client = createClient({
             url: String(process.env.REDIS_URL) || `redis://${RedisConfig.Host}:${RedisConfig.Port}`,
-            username: RedisConfig.Username,
-            password: RedisConfig.Password,
             legacyMode: process.env.NODE_ENV !== 'test',
         });
         this.client
