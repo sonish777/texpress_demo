c77d1d9df2e5bc701071b9398e9618439c3f1811
Sonish <sonishmaharjan1@gmail.com>
2023-04-20 23:11:41 +0545

    fix: fix cms url config

--- apps/texpress-cms/src/configs/server.config.ts
+++ apps/texpress-cms/src/configs/server.config.ts
@@ -1,6 +1,8 @@
 import config from 'config';
 
 export const ServerConfig = {
-    URL: `${config.get('server.cms:host')}:${config.get('server.cms:port')}`,
+    URL:
+        process.env.CMS_URL ||
+        `${config.get('server.cms:host')}:${config.get('server.cms:port')}`,
     PORT: process.env.CMS_PORT || config.get('server.cms:port'),
 };
