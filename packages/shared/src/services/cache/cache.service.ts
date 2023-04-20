f457aea6977d1e56e726c80760be7630791500c7
Sonish <sonishmaharjan1@gmail.com>
2023-04-20 14:54:39 +0545

    fix: add prod server url and port in config

--- packages/shared/src/services/cache/cache.service.ts
+++ packages/shared/src/services/cache/cache.service.ts
@@ -4,34 +4,34 @@ import { isJSON } from 'shared/utils';
 
 @Service()
 export class CacheService {
-    // set(key: string, value: Record<string, any>, ttl?: number): Promise<any>;
-    // set(key: string, value: string, ttl?: number): Promise<any>;
-    // set(key: string, value: number, ttl?: number): Promise<any>;
-    // set(key: string, value: any, ttl = redis.ttl): Promise<any> {
-    //     // if (typeof value === 'string') {
-    //     //     return redis.client.v4.set(key, value, {
-    //     //         EX: ttl,
-    //     //     });
-    //     // } else if (typeof value === 'object') {
-    //     //     return redis.client.v4.set(key, JSON.stringify(value), {
-    //     //         EX: ttl,
-    //     //     });
-    //     // }
-    //     // return redis.client.v4.set(String(key), JSON.stringify(value), {
-    //     //     EX: ttl,
-    //     // });
-    // }
+    set(key: string, value: Record<string, any>, ttl?: number): Promise<any>;
+    set(key: string, value: string, ttl?: number): Promise<any>;
+    set(key: string, value: number, ttl?: number): Promise<any>;
+    set(key: string, value: any, ttl = redis.ttl): Promise<any> {
+        if (typeof value === 'string') {
+            return redis.client.set(key, value, {
+                EX: ttl,
+            });
+        } else if (typeof value === 'object') {
+            return redis.client.set(key, JSON.stringify(value), {
+                EX: ttl,
+            });
+        }
+        return redis.client.set(String(key), JSON.stringify(value), {
+            EX: ttl,
+        });
+    }
 
-    // async get<K>(key: string): Promise<K | null> {
-    //     // const data = await redis.client.v4.get(key);
-    //     // if (!data) {
-    //     //     return null;
-    //     // }
-    //     // if (isJSON(data)) {
-    //     //     return JSON.parse(data) as K;
-    //     // }
-    //     // return data as K;
-    // }
+    async get<K>(key: string): Promise<K | null> {
+        const data = await redis.client.get(key);
+        if (!data) {
+            return null;
+        }
+        if (isJSON(data)) {
+            return JSON.parse(data) as K;
+        }
+        return data as K;
+    }
 
     delete(key: string) {
         return redis.client.del(key);
