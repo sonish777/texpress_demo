476d7ade9851c80df4fb8c2a13b9fca57939313c
Sonish <sonishmaharjan1@gmail.com>
2023-04-21 14:00:36 +0545

    fix: added logs for testing

--- apps/consumer/src/controllers/health.controller.ts
+++ apps/consumer/src/controllers/health.controller.ts
@@ -1,10 +1,35 @@
 import { Controller, Route } from 'core/controllers';
 import { HTTPMethods } from 'core/utils';
 import { Request, Response } from 'express';
+import { readdirSync } from 'fs';
+import path from 'path';
 @Controller('/healthz')
 export class HealthController {
     @Route({ method: HTTPMethods.Get, path: '/' })
     async getHealth(req: Request, res: Response) {
+        
+        console.log(
+            '--------------Avatars------------------',
+            readdirSync(
+                path.join(
+                    __dirname,
+                    '../../../texpress-cms/public/uploads',
+                    'admins'
+                )
+            )
+        );
+
+        console.log(
+            '------------------Thumbnails-------------',
+            readdirSync(
+                path.join(
+                    __dirname,
+                    '../../../texpress-cms/public/uploads',
+                    'admins',
+                    'thumbnails'
+                )
+            )
+        );
         return res.status(200).json({
             status: 'Running',
             code: 200,
