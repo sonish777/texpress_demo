b93f8e615f5ec2640128f8ae286da96bdee4a818
Sonish <sonishmaharjan1@gmail.com>
2023-04-21 14:59:18 +0545

    fix: remove console logs

--- apps/consumer/src/controllers/health.controller.ts
+++ apps/consumer/src/controllers/health.controller.ts
@@ -1,23 +1,11 @@
 import { Controller, Route } from 'core/controllers';
 import { HTTPMethods } from 'core/utils';
 import { Request, Response } from 'express';
-import { existsSync, mkdirSync, readdirSync } from 'fs';
-import path from 'path';
+
 @Controller('/healthz')
 export class HealthController {
     @Route({ method: HTTPMethods.Get, path: '/' })
     async getHealth(req: Request, res: Response) {
-        console.log(
-            '--------------Avatars------------------',
-            readdirSync(
-                path.join(
-                    __dirname,
-                    '../../../texpress-cms/public/uploads',
-                    'admins'
-                )
-            )
-        );
-
         return res.status(200).json({
             status: 'Running',
             code: 200,
